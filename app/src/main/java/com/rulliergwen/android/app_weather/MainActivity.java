c07984027a18cc4130607f6cf77896d66090e56b
Gwen-Rullier <laurent.gwenaelle@gmail.com>
2020-08-31 17:36:21 +0200

    Fin 1ere journée

--- app/src/main/java/com/rulliergwen/android/app_weather/MainActivity.java
+++ app/src/main/java/com/rulliergwen/android/app_weather/MainActivity.java
@@ -3,18 +3,30 @@ package com.rulliergwen.android.app_weather;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView mTextViewCityName;
+    private TextView mtextViewNoConnect;
+    private LinearLayout mLinearLayoutCurrentCity;
+    private Button mButton;
+    private Context mContext;
+    private FloatingActionButton mFloatingButtonFavorite ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
@@ -24,17 +36,31 @@ public class MainActivity extends AppCompatActivity {
         Toast.makeText(this, mTextViewCityName.getText(), Toast.LENGTH_SHORT).show();
 
         //TP2 : l connexion
+        mLinearLayoutCurrentCity = (LinearLayout) findViewById(R.id.linear_layout_current_city);
+        mtextViewNoConnect = (TextView) findViewById(R.id.text_view_noConnect);
+
         ConnectivityManager connMgr = (ConnectivityManager) getSystemService(
                 Context.CONNECTIVITY_SERVICE);
         NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
         if (networkInfo != null && networkInfo.isConnected()) {
             // Oui je suis connecté à internet
-            setContentView(R.layout.activity_main);
+            mLinearLayoutCurrentCity.setVisibility(View.VISIBLE);
+            mtextViewNoConnect.setVisibility(View.INVISIBLE);
         } else {
             // Non...
-            
+            mLinearLayoutCurrentCity.setVisibility(View.INVISIBLE);
+            mtextViewNoConnect.setVisibility(View.VISIBLE);
         }
+        mFloatingButtonFavorite.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(mContext, FavoriteActivity.class);
+                startActivity(intent);
+            }
+        });
 
 
     }
+
+
 }
